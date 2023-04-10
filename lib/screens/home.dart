66fd6de9ef34ac9589489785650b07475771090e
H1MSK <ksda47832338@outlook.com>
2023-04-10 17:18:10 +0800

    impl: home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:dashboard/widgets/clients.dart';
+import 'package:dashboard/widgets/page.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,9 +9,15 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with PageMixin {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ScaffoldPage.scrollable(
+        header: const PageHeader(title: Text("Home")),
+        children: [
+          subtitle(content: const Text("Clients")),
+          spacer,
+          ClientsList(ClientsList.nonFilter)
+        ]);
   }
 }
